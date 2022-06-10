88ffded710490707ba6c88fd4b5bc4cd5efb351e
Prince Jan Kumi <janprince002@gmail.com>
2022-06-10 18:31:23 +0000

    add static files and templates

--- msystem/views.py
+++ msystem/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+@login_required(login_url='/login/')
 def index(request):
+    return render(request, "msystem/add_client.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("useremail")
+        password = request.POST.get("password")
+        user = User.objects.get(email=email)
+        user = authenticate(request, username=user.username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("msystem:index"))
+        else:
+            context = {
+                'error': "Invalid email or password",
+            }
+            return render(request, "msystem/login.html", context=context)
     return render(request, "msystem/login.html")
 
 
-
-
-def login(request):
-    pass
-
-
 def signup(request):
-    pass
+    return render(request, "msystem/register.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("msystem:login"))
