d521b53c39741b6cbba2e49775be34d8266dd128
Prince Jan Kumi <janprince002@gmail.com>
2022-06-12 21:57:43 +0000

    add view for add_person

--- msystem/urls.py
+++ msystem/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "msystem"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("clients/add", views.add_person, name="add_person"),
     path("login/", views.login_view, name='login'),
     path("logout/", views.logout_view, name='logout'),
 ]
--- msystem/views.py
+++ msystem/views.py
@@ -4,13 +4,69 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.dateparse import parse_date
+from .models import *
 
 
 # Create your views here.
 @login_required(login_url='/login/')
 def index(request):
-    return render(request, "msystem/add_client.html")
+    return render(request, "msystem/add_person.html")
 
+
+def add_person(request):
+    if request.method == "GET":
+        print(request.GET)
+        full_name = request.GET["name"]
+        dob = request.GET["dob"]
+        phone = request.GET["phone"]
+        telephone = request.GET["telephone"]
+        profession = request.GET["profession"]
+        residence = request.GET["name"]
+        country = request.GET["country"]
+        hometown = request.GET['hometown']
+        religion = request.GET["religion"]
+        request_info = request.GET["request_info"]
+        remark = request.GET["remark"]
+        solution = request.GET["solution"]
+        roles = request.GET.getlist("roles")
+
+        # parse date
+        dob = parse_date(dob)
+
+        # Create a model instance
+        p = Person(full_name=full_name,
+                   phone=phone,
+                   date_of_birth=dob,
+                   telephone=telephone,
+                   profession=profession,
+                   residence=residence,
+                   country=country,
+                   hometown=hometown,
+                   religion=religion,
+                   request_info=request_info,
+                   remark=remark,
+                   solution=solution
+            )
+
+        if 'student' in roles: p.is_student = True
+        if 'employee' in roles: p.is_employee = True
+        if 'client' in roles: p.is_client = True
+        if 'rep' in roles: p.is_rep = True
+        p.save()
+
+        if 'employee' in roles:
+            # create new employee
+            e = Employee(person=p)
+            e.save()
+
+        if 'rep' in roles:
+            # create new rep
+            r = Rep(person=p)
+            r.save()
+        return render(request, "msystem/add_client.html")
+    else:
+        return render(request, "msystem/add_client.html")
 
 def login_view(request):
     if request.method == "POST":
